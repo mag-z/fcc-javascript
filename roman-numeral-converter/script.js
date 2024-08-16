075c75d982f284ea6e1635c6420f87b9c6d16475
yzou235 <yumingzou235@gmail.com>
2024-08-16 08:53:59 -0400

    complete function decimalToRoman(number). Next step is to build html

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -1,23 +1,27 @@
-const decimalToRoman = (num) => {
-    
-    if (number <= 0) {
+const checkNum = (num) => {
+    if (num <= 0) {
         console.log("Please enter a number greater or equal to 1");
         return;
-    } else if (number > 3999) {
+    } else if (num > 3999) {
         console.log("Please enter a number less than or equal to 3999");
         return;
     }
-
-    let symbol = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
-    let value = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-
-    let i = 12;
-    let result = ""
-
-    while (i >= 0) {
-        let quotient = Math.floor(number/symbol[i]);
-        let remainder = number % symbol[i];
-        result += value[i] * remainder;
-        
-    }
 }
+
+let symbol = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+let value = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+
+const decimalToRoman = (number) => {
+    let quotient;
+    let result = "";
+    for (let i = 12; i >= 0; i--) {
+        while(number >= value[i]) {
+            quotient = Math.floor(number/value[i]);
+            result += symbol[i].repeat(quotient);
+            number -= value[i] * quotient;
+        }
+    }
+    return result;
+}
+
+console.log(decimalToRoman(3999));
