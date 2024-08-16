22dea72f837581b1ab79f7d153b9fb5bccd3b0f7
yzou235 <yumingzou235@gmail.com>
2024-08-15 23:48:40 -0400

    created html, css, and script files. Start working on function decimalToRoman.

---
+++ roman-numeral-converter/script.js
@@ -0,0 +1,23 @@
+const decimalToRoman = (num) => {
+    
+    if (number <= 0) {
+        console.log("Please enter a number greater or equal to 1");
+        return;
+    } else if (number > 3999) {
+        console.log("Please enter a number less than or equal to 3999");
+        return;
+    }
+
+    let symbol = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+    let value = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+
+    let i = 12;
+    let result = ""
+
+    while (i >= 0) {
+        let quotient = Math.floor(number/symbol[i]);
+        let remainder = number % symbol[i];
+        result += value[i] * remainder;
+        
+    }
+}
--- roman-numeral-converter/userStories.md
+++ roman-numeral-converter/userStories.md
@@ -13,3 +13,5 @@
 9. When the #number element contains the number 649 and the #convert-btn element is clicked, the #output element should contain the text "DCXLIX"
 10. When the #number element contains the number 1023 and the #convert-btn element is clicked, the #output element should contain the text "MXXIII"
 11. When the #number element contains the number 3999 and the #convert-btn element is clicked, the #output element should contain the text "MMMCMXCIX"
+
+**Example**: https://roman-numeral-converter.freecodecamp.rocks/
