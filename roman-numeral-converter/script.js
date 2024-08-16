2a1baad93b8b577c8a97105f05474f1a3f827d48
yzou235 <yumingzou235@gmail.com>
2024-08-16 19:59:35 -0400

    Finished html and javascript. Will work on css styling.

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -1,17 +1,32 @@
-const checkNum = (num) => {
-    if (num <= 0) {
-        console.log("Please enter a number greater or equal to 1");
-        return;
-    } else if (num > 3999) {
-        console.log("Please enter a number less than or equal to 3999");
+const inputNumber = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const outputText = document.getElementById("output");
+
+const checkConvertInput = () => {
+    const num = Number(inputNumber.value)
+
+    if (!Number.isInteger(num) || isNaN(num)) {
+        outputText.innerText = "Please enter a valid number";
         return;
     }
+
+    if (num <= 0) {
+        outputText.innerText = "Please enter a number greater or equal to 1";
+        return;
+    } else if (num > 3999) {
+        outputText.innerText = "Please enter a number less than or equal to 3999";
+        return;
+    }
+
+    decimalToRoman(num);
 }
+
+// check int "Please enter a valid number" "
 
 let symbol = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 let value = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
-const decimalToRoman = (number) => {
+const decimalToRoman = (number) => {    
     let quotient;
     let result = "";
     for (let i = 12; i >= 0; i--) {
@@ -21,7 +36,7 @@ const decimalToRoman = (number) => {
             number -= value[i] * quotient;
         }
     }
-    return result;
+    outputText.innerText = result;
 }
 
-console.log(decimalToRoman(3999));
+convertBtn.addEventListener("click", checkConvertInput);
