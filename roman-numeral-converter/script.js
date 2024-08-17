c03b41eef3917886794b0fce53732c6f2be98708
yzou235 <yumingzou235@gmail.com>
2024-08-16 22:38:34 -0400

    last commit

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -1,27 +1,7 @@
 const inputNumber = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
+const resetBtn = document.getElementById("reset-btn");
 const outputText = document.getElementById("output");
-
-const checkConvertInput = () => {
-    const num = Number(inputNumber.value)
-
-    if (!Number.isInteger(num) || isNaN(num)) {
-        outputText.innerText = "Please enter a valid number";
-        return;
-    }
-
-    if (num <= 0) {
-        outputText.innerText = "Please enter a number greater or equal to 1";
-        return;
-    } else if (num > 3999) {
-        outputText.innerText = "Please enter a number less than or equal to 3999";
-        return;
-    }
-
-    decimalToRoman(num);
-}
-
-// check int "Please enter a valid number" "
 
 let symbol = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
 let value = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
@@ -36,7 +16,40 @@ const decimalToRoman = (number) => {
             number -= value[i] * quotient;
         }
     }
-    outputText.innerText = result;
+    outputText.innerHTML = `<p class="result">${result}</p>`;
 }
+
+
+const checkConvertInput = () => {
+    const num = Number(inputNumber.value)
+
+    if (!Number.isInteger(num) || isNaN(num)) {
+        outputText.innerHTML = `<p class="alert">Please enter a valid number</p>`;
+        return;
+    }
+
+    if (num <= 0) {
+        outputText.innerHTML = `<p class="alert">Please enter a number greater or equal to 1</p>`;
+        return;
+    } else if (num > 3999) {
+        outputText.innerHTML = `<p class="alert">Please enter a number less than or equal to 3999</p>`;
+        return;
+    }
+
+    decimalToRoman(num);
+}
+
+inputNumber.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkConvertInput();
+    }
+});
+
+const reset = () => {
+    inputNumber.value = "";
+    outputText.innerHTML = "";
+}
+
+resetBtn.addEventListener("click", reset);
 
 convertBtn.addEventListener("click", checkConvertInput);
