f80332bcd90facf6cff8164004fd63a9cefcabb2
yzou235 <yumingzou235@gmail.com>
2024-09-07 07:31:19 -0400

    created and tested validatePhoneNumber function.

--- phone-number-validator/script.js
+++ phone-number-validator/script.js
@@ -4,18 +4,23 @@ const clearBtn = document.getElementById("clear-btn");
 const resultDiv = document.getElementById("result-div");
 
 // create phone pattern regex
-const countryCode = ;
-const areaCode = ;
-const localCode = ;
-const phoneRegex = `${countryCode}${areaCode}${localCode}`;
+const countryCode = "(1\\s?)?";
+const areaCode = "(?:[0-9]{3}|\\([0-9]{3}\\))";
+const dashSpace = "[-\\s]?"
+const localCode = "[0-9]{3}[-\\s]?[0-9]{4}";
+const phoneRegex = new RegExp(`^${countryCode}${areaCode}${dashSpace}${localCode}$`);
 
-const validatePhoneNumber = () => {
+const validatePhoneNumber = (str) => {
     // check if empty input
-    if (userInput === "") {
+    if (str === "") {
         alert('Please provide a phone number');
         return;
     }
-    
+    if (phoneRegex.test(str)) {
+        console.log("Valid US number: ")
+    } else {
+        console.log("Invalid US number: ")
+    }
 }
 
 const displayResult = () => {
